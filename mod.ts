a018494eca50a957e0848680008b468bac7d65a9
Luke Shay <38476395+LukeShay@users.noreply.github.com>
2020-09-28 19:48:26 +0000

    revert: removed VERSION from module

--- mod.ts
+++ mod.ts
@@ -1,8 +1,6 @@
 // Copyright 2020 Luke Shay. All rights reserved. MIT license.
 /* @module lapi */
 
-export const VERSION = "0.0.3";
-
 export * from "./lib/lapi.ts";
 export * from "./lib/lapi_error.ts";
 export * from "./lib/lapi_router.ts";

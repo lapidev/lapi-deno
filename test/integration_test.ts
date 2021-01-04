c3f16b5ced54c38103f09a6139535f646df4c45c
Luke Shay <shay.luke17@gmail.com>
2021-01-04 16:02:54 -0600

    kill existing process

--- test/integration_test.ts
+++ test/integration_test.ts
@@ -6,6 +6,20 @@ import { RequestMethod } from "../lib/lapi_route.ts";
 Deno.test({
   name: "Lapi integration test",
   fn: async () => {
+    const pidProc = Deno.run({ cmd: ["lsof", "-i", ":3000"], stdout: "piped" });
+
+    const pid = new TextDecoder("utf-8").decode(await pidProc.output()).match(
+      /\d\d\d\d\d/,
+    );
+
+    if (pid && pid[0]) {
+      const kill = Deno.run({ cmd: ["kill", pid[0]] });
+      await delay(200);
+      kill.close();
+    }
+
+    pidProc.close();
+
     const api = Deno.run(
       { cmd: ["deno", "run", "--allow-net", "test/api.ts"] },
     );

95dc72ac2540fe348856840a11372666894b61df
Luke Shay <38476395+LukeShay@users.noreply.github.com>
2021-06-17 14:46:38 +0000

    chore: added example

---
+++ examples/basic_api.ts
@@ -0,0 +1,55 @@
+// Copyright 2020 Luke Shay. All rights reserved. MIT license.
+
+import {
+  Application,
+  LapiRequest,
+  RequestMethod,
+  Response,
+  ServerRequest,
+} from "../mod.ts";
+import { assertEquals } from "https://deno.land/std@0.83.0/testing/asserts.ts";
+import { LapiResponse } from "../lib/lapi_response.ts";
+
+const application = new Application({ timer: true });
+
+application.get(
+  "/hello/<name>",
+  (request: LapiRequest, response: LapiResponse): void => {
+    response.send({ body: `Hello, ${request.params.name}!` });
+  },
+);
+
+application.get("/json", (request: LapiRequest, response: LapiResponse): void => {
+  response.json({ hello: "This is JSON" }).send();
+});
+
+application.get(
+  "/xml",
+  (request: LapiRequest, response: LapiResponse): void => {
+    response.xml("<tag>This is some XML</tag>").send();
+  },
+);
+
+if (Deno.env.get("DENO_ENV") !== "TEST") {
+  await application.start(() => {
+    console.log(
+      `Server started on http://${application.serverHost}:${application.serverPort}`,
+    );
+  });
+}
+
+Deno.test('/hello should return { body: "Hello!" }', async () => {
+  const responses: unknown[] = [];
+
+  const serverRequest = {
+    method: RequestMethod.GET,
+    url: "/hello",
+    respond: (res: Response) => {
+      responses.push(res);
+    },
+  } as unknown as ServerRequest;
+
+  await application.handleRequest(serverRequest);
+
+  assertEquals(responses[0], { body: "Hello!" });
+});
--- get.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-
-curl https://cdn.deno.land/lapi/versions/0.2.3/raw/$1 -o $1
